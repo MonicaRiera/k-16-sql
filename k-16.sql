1e2769428a5180c16a4f423d37fd7f66d5ab4d0a
Monica <monicarigi@hotmail.com>
2019-09-19 12:24:18 +0700

    160103

--- k-16.sql
+++ k-16.sql
@@ -25,3 +25,23 @@ VALUES ('John Smith', 'john@email.com'),
 ('Bob Adams', 'bob@email.com')
 
 --160103
+
+CREATE TABLE places (
+    placeId int NOT NULL AUTO_INCREMENT,
+    title varchar(255) NOT NULL,
+    price int NOT NULL,
+    rating int,
+    city varchar(255) NOT NULL,
+    type int NOT NULL,
+    host int NOT NULL,
+		PRIMARY KEY(placeId)
+)
+
+INSERT INTO places (title, price, rating, city, type, host)
+VALUES
+('Amazing Villa by the Ocean', 150, 5, 'Phuket', 1, 2),
+('Amazing View in the Jungle', 80, 4, 'Phangan', 2, 1),
+('Villa with 360 Rooftop', 160, 5, 'Samui', 1, 1),
+('Cool flat in trendy area', 80, 5, 'Chiang Mai', 2, 4),
+('Stylish room in city center', 50, 3, 'Bangkok', 3, 2),
+('Cozy room by the beach', 40, 4, 'Samui', 3, 1)
