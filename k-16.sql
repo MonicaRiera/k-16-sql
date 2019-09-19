71ac544bdd9151d2ce913385bb1241a145782c61
Monica <monicarigi@hotmail.com>
2019-09-19 12:10:22 +0700

    160102

--- k-16.sql
+++ k-16.sql
@@ -10,3 +10,18 @@ INSERT INTO types (name)
 VALUES ('Entire Villa'), ('Entire House'), ('Shared House')
 
 --160102
+
+CREATE TABLE users (
+  userId int NOT NULL AUTO_INCREMENT,
+  name varchar(255),
+  email varchar(255),
+  PRIMARY KEY (userId)
+)
+
+INSERT INTO users (name, email)
+VALUES ('John Smith', 'john@email.com'),
+('Sarah Black', 'sarah@gmail.com'),
+('Albert Philips', 'albert@gmail.com'),
+('Bob Adams', 'bob@email.com')
+
+--160103
