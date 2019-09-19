c06ec021c7ab0e883a95a9595c3a48e156b22d58
Monica <monicarigi@hotmail.com>
2019-09-19 12:03:24 +0700

    160101

---
+++ k-16.sql
@@ -0,0 +1,10 @@
+--160101
+
+CREATE TABLE types (
+  typeId int NOT NULL AUTO_INCREMENT,
+  name varchar(255),
+	PRIMARY KEY (typeId)
+)
+
+INSERT INTO types (name)
+VALUES ('Entire Villa'), ('Entire House'), ('Shared House')
